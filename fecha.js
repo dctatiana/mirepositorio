ec3bc12f1a691ce96fba7701582885c41e4c6d98
tatiana <dctatiana@hotmail.com>
2023-02-14 16:37:37 -0300

    cv

--- fecha.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('boton'). onclick = function () {
-    console.log ("capturamos el click")
-        document.getElementById("demo").innerText = "contendio cambiado"; 
-      }
-      
