f01c6c24ce7573fb9e4cb1d43b33c3a6fed65345
tatiana <dctatiana@hotmail.com>
2023-02-19 15:06:39 -0300

    cvtatianadacosta

---
+++ iconos.js
@@ -0,0 +1,12 @@
+document.getElementById('bot'). onclick = function () {
+    console.log ("capturamos el click")
+        document.getElementById("dem").innerText ="1564794747"; 
+      }
+      document.getElementById('bot2'). onclick = function () {
+        console.log ("capturamos el click")
+            document.getElementById("dem2").innerText ="dctatiana@hotmail.com"; 
+          }   
+          document.getElementById('bot3'). onclick = function () {
+            console.log ("capturamos el click")
+                document.getElementById("dem3").innerText ="@tatianadc"; 
+              }
